9f172525d283d753dcaa4bf185a1ec5cead04748
Loris Tarazi <80705867+Loris99@users.noreply.github.com>
2023-02-11 12:49:38 +0200

    edit text

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -6,16 +6,16 @@ const MainPage = () => {
     <div className={classes.mainContainer}>
       <h1 className={classes.title}>Welcome to my little world</h1>
       <div className={classes.intro}>
-        Hey there! My name is Loris Tarazi, and I'm all about so many things!{" "}
+        Hey there! My name is Loris Tarazi, and I'm all about so many things!
         <br />
-        In this side of my world; I'm an aspiring web-developer. My first step
-        was in the Front-End development, I've been walking this path for almost
-        two years now!, little younger than covid-19. I have learned & grown in
-        it immensely, and I'm constantly on the hunt for more things to learn.
-        <br /> This gallery is about showing my journey and how I have evolved
-        and still am; starting from the first project, still there are some that
-        are under construction! <br /> These are small projects I did for fun..
-        I hope you enjoy them the same way I did while creating them!
+        In this side of my world; I'm a web-developer. My first step was in the
+        Front-End development, and I've been walking this path for almost two
+        years now! a little younger than covid-19. <br /> I have learned & grown
+        in it immensely, and I'm constantly on the hunt for more things to
+        learn.
+        <br /> This gallery is about showing my journey and some of the projects
+        I did along the way for fun..
+        <br /> I hope you enjoy them the same way I did while creating them!
       </div>
       <Card />
     </div>
