b0001cdb246b145c01aca7a16334da2a42a7aa4f
Loris Tarazi <80705867+Loris99@users.noreply.github.com>
2023-02-12 15:18:03 +0200

    touch ups

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import './App.css';
-// import { Routes, Route } from "react-router-dom";
 import MainPage from "./Components/MainPage/MainPage";
 import React from 'react';
 
 function App  ()  {
   return <MainPage/>
   ;
-//  <h1>Hello!</h1>
  
 }
 
--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,67 +1,56 @@
 import React from "react";
 import styles from "./Card.module.css";
-// import calendar from "./../../Assets/calendar.png";
-// import lock from "./../../Assets/lock.png";
-import mystery from "./../../Assets/mystery.png";
-import teaCup from "./../../Assets/tea-cup.png";
 
 const Card = () => {
   return (
     <div className={styles.cardsContainer}>
       <div className={styles.twoCardsLine}>
-          <div className={styles.flipCard}>
-            <div className={styles.flipCardInner}>
-              <div className={styles.flipCardFront}>
-                <h1>Event Planner</h1>
+        <div className={styles.flipCard}>
+          <div className={styles.flipCardInner}>
+            <div className={styles.flipCardFront}>
+              <h1>Event Planner</h1>
+            </div>
+            <div className={styles.flipCardBack}>
+              <div className={styles.cardContent}>
+                <p>
+                  Check all of the events happening around you!
+                  <br />A full functional javascript task; add/edit/delete
+                  events connected with realtime database! <br />
+                  In this project I was more focused on grasping & understanding
+                  javaScript concepts
+                </p>
+                <a
+                  className={styles.checkmebtn}
+                  href="https://loris99.github.io/events-around-me/"
+                >
+                  Check Me
+                </a>
               </div>
-              <div className={styles.flipCardBack}>
-                <div className={styles.cardContent}>
-                  <p>
-                    Check all of the events happening around you!
-                    <br />A full functional javascript task; add/edit/delete
-                    events connected with realtime database! <br />
-                    In this project I was more focused on grasping &
-                    understanding javaScript concepts
-                  </p>
-                  <a
-                    className={styles.checkmebtn}
-                    href="https://loris99.github.io/events-around-me/"
-                  >
-                    Check Me
-                  </a>
-                </div>
             </div>
           </div>
         </div>
-          <div className={styles.flipCard}>
-            <div className={styles.flipCardInner}>
-              <div className={styles.flipCardFront}>
-                <h1>The Tea Shop</h1>
-                {/* <img
-                  style={{
-                    width: "calc(20% + 20 * ((100vw — 500px) / (1420)))",
-                  }}
-                  src={teaCup}
-                  alt="tea cup "
-                /> */}
-              </div>
-              <div className={styles.flipCardBack}>
-                <div className={styles.cardContent}>
-                  <p>
-                    I share my love for tea and its varieties through this
-                    website; which demonstrates the analysis of a fictional cozy
-                    tea shop, in which I learned how to use charts in react!
-                    <br /> ps: I love coffee too..
-                  </p>
-                  <a
-                    className={styles.checkmebtn}
-                    href="https://loris99.github.io/tea-shop/"
-                  >
-                    Check Me
-                  </a>
-                </div>
+        <div className={styles.flipCard}>
+          <div className={styles.flipCardInner}>
+            <div className={styles.flipCardFront}>
+              <h1>The Tea Shop</h1>
+            </div>
+            <div className={styles.flipCardBack}>
+              <div className={styles.cardContent}>
+                <p>
+                  I share my love for tea and its varieties through this
+                  website; which demonstrates the analysis of a fictional cozy
+                  tea shop, in which I learned how to use charts in react!
+                  <br /> ps: I love coffee too..
+                </p>
+                <a
+                  className={styles.checkmebtn}
+                  href="https://loris99.github.io/tea-shop/"
+                >
+                  Check Me
+                </a>
               </div>
             </div>
+          </div>
         </div>
       </div>
       <div className={styles.oneCardLine}>
@@ -90,41 +79,33 @@ const Card = () => {
         </div>
       </div>
       <div className={styles.twoCardsLine}>
-          <div className={styles.flipCard}>
-            <div className={styles.flipCardInner}>
-              <div className={styles.flipCardFront}>
-                <h1>Health Care Search & Reviews</h1>
+        <div className={styles.flipCard}>
+          <div className={styles.flipCardInner}>
+            <div className={styles.flipCardFront}>
+              <h1>Health Care Search & Reviews</h1>
+            </div>
+            <div className={styles.flipCardBack}>
+              <div className={styles.cardContent}>
+                <p>
+                  Search for a specialist by name, speciality or location, all
+                  of which are registered in your healthcare from 'al-mashreq',
+                  not only that but you can also check their reviews and ratings
+                  and leave your own! <br /> you need to disable CORS..
+                </p>
+                <a
+                  className={styles.checkmebtn}
+                  href="https://loris99.github.io/health-care-info/"
+                >
+                  Check Me
+                </a>
               </div>
-              <div className={styles.flipCardBack}>
-                <div className={styles.cardContent}>
-                  <p>
-                    Search for a specialist by name, speciality or location, all
-                    of which are registered in your healthcare from
-                    'al-mashreq', not only that but you can also check their
-                    reviews and ratings and leave your own! <br /> you need to
-                    disable CORS..
-                  </p>
-                  <a
-                    className={styles.checkmebtn}
-                    href="https://loris99.github.io/health-care-info/"
-                  >
-                    Check Me
-                  </a>
-                </div>
-              </div>
+            </div>
           </div>
         </div>
         <div className={styles.flipCard}>
           <div className={styles.flipCardInner}>
             <div className={styles.flipCardFront}>
               <h1>Mystery Project</h1>
-              {/* <img
-                  style={{
-                    width: "calc(20% + 20 * ((100vw — 500px) / (1420)))",
-                  }}
-                  src={mystery}
-                  alt="mystery box"
-                /> */}
             </div>
             <div className={styles.flipCardBack}>
               <div className={styles.cardContent}>
